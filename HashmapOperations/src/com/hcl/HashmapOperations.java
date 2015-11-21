40067fbdec5e49ea96364fe9bf54c471d9d0df5d
prasadkp <prasadkp@DS-448A5BBFFBAE.HCLT.CORP.HCL.IN>
2015-11-21 14:30:30 +0530

    simplechanges

--- HashmapOperations/src/com/hcl/HashmapOperations.java
+++ HashmapOperations/src/com/hcl/HashmapOperations.java
@@ -11,9 +11,9 @@ public class HashmapOperations {
 		HashMap<String, Integer> hashMap = new HashMap<>();
 		hashMap.put("jan", 1);
 		hashMap.put("feb", 2);
-		hashMap.put("mar", 3);
 		hashMap.put("apr", 4);
 		hashMap.put("may", 5);
+		hashMap.put("mar", 3);
 
 		System.out.println(hashMap);
 
